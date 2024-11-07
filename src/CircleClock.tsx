4691cc7f8d933b1f4242dcdf5de5071b77b422d0
wonggclw <gcw@groundworksdesign.com>
2024-11-07 10:28:41 -0700

    Starter code created

---
+++ src/CircleClock.tsx
@@ -0,0 +1,63 @@
+import { useState, useEffect, FC } from "react";
+import * as d3 from "d3";
+
+interface CircleProps {
+  color: string;
+  screenWidth: number;
+  screenHeight: number;
+}
+
+const CircleClock: FC<CircleProps> = ({ color, screenWidth, screenHeight }) => {
+  const clockRadius = 200,
+    margin = 50,
+    w = (clockRadius + margin) * 2,
+    h = (clockRadius + margin) * 2,
+    hourHandLength = (2 * clockRadius) / 3,
+    minuteHandLength = clockRadius,
+    secondHandLength = clockRadius - 12,
+    secondHandBalance = 30,
+    secondTickStart = clockRadius,
+    secondTickLength = -10,
+    hourTickStart = clockRadius,
+    hourTickLength = -18,
+    secondLabelRadius = clockRadius + 16,
+    secondLabelYOffset = 5,
+    hourLabelRadius = clockRadius - 40,
+    hourLabelYOffset = 7,
+    radians = Math.PI / 180;
+
+  useEffect(() => {
+    drawCanvas(screenWidth, screenHeight);
+    drawClock();
+
+    const timeInterval = setInterval(() => {
+      updateData();
+      moveHands();
+    }, 1000);
+
+    return () => {
+      clearInterval(timeInterval);
+      // add line to clear the svg from the screen
+    };
+  }, []);
+
+  return <div id="container">If you can read this, you can read!</div>;
+
+  function drawClock() {
+    // stuff here!
+  }
+
+  function moveHands() {
+    // stuff here!
+  }
+
+  function updateData() {
+    // stuff here!
+  }
+};
+
+export default CircleClock;
+
+function drawCanvas(width: number, height: number) {
+  // stuff here!
+}
